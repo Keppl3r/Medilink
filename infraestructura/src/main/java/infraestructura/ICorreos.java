de9d558537e20a4a899a6185c177054a81f3fe22
Keppl3r <kevin.mendoza215752@potros.itson.edu.mx>
2026-04-13 19:36:46 -0400

    ICorreos y SS_Notificaciones creados en infraestructura

---
+++ infraestructura/src/main/java/infraestructura/ICorreos.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package infraestructura;
+
+/**
+ * Interfaz para el servicio de envio de correos electronicos. Define el
+ * contrato para notificaciones por correo.
+ *
+ * @author Adrian Mendoza
+ */
+public interface ICorreos {
+    /**
+     * Envia un correo de confirmacion al paciente.
+     *
+     * @param correo direccion de correo del destinatario
+     * @param mensaje contenido del mensaje a enviar
+     */
+    void enviarConfirmacion(String correo, String mensaje);
+}
