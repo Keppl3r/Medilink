0d72dd64f2538619a94d5b89e89f4a3c6c374aa6
Keppl3r <kevin.mendoza215752@potros.itson.edu.mx>
2026-04-13 19:41:36 -0400

    Agregando AgendadorCitas

---
+++ negocio/src/main/java/negocio/AgendadorCitas.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package negocio;
+
+import com.mycompany.persistencia.entidades.Cita;
+import dto.DTO_Cita;
+import excepciones.NegocioException;
+
+/**
+ *
+ * @author keppler
+ */
+public class AgendadorCitas implements IAgendadorCita{
+
+    @Override
+    public Cita agendarCita(DTO_Cita dto) throws NegocioException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
